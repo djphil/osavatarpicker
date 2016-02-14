d2d73de40527f5d1ab06fa353f6c8ac23e90da29
djphil <digitalconcepts@free.fr>
2016-02-14 04:14:33 +0100

    clean up code
    clean up code

--- inc/avatars_list.php
+++ inc/avatars_list.php
@@ -7,15 +7,14 @@ $query = $db->prepare("
     WHERE agentID = '".$MasterAvatarUUID."'
     AND folderName = 'wear_folder' 
 ");
-        
+
 $query->execute();
 $counter = $query->rowCount();
-    
+
 if ($counter == 0)
 {
-    // echo '<p class="alert red">0 folder "wear_folder" found ...</p>';
-    // exit;
-    $_SESSION['flash']['danger'] = "0 folder \"wear_folder\" found ...";
+    echo '<p class="alert alert-danger alert-anim">0 folder "wear_folder" found ...</p>';
+    exit;
 }
 
 while ($row = $query->fetch(PDO::FETCH_ASSOC))
@@ -29,13 +28,13 @@ while ($row = $query->fetch(PDO::FETCH_ASSOC))
         FROM inventoryfolders
         WHERE parentFolderID = '".$folderID."'
     ");
-            
+
     $query->execute();
     $counter = $query->rowCount();
-        
+
     if ($counter == 0)
     {
-        echo '<p class="alert red">0 destination found ...</p>';
+        echo '<p class="alert alert-danger alert-anim">0 Avatar folder found ...</p>';
         exit;
     }
 
@@ -52,27 +51,16 @@ while ($row = $query->fetch(PDO::FETCH_ASSOC))
         echo '<a href="secondlife:///app/wear_folder/?folder_id='.$parentFolderID.'" target="_self" style="text-decoration: none;">';
         echo '<img class="img-thumbnail" src="img/'.$folderName.'.jpg" alt="'.$folderName.'" >';
         echo '<div class="regions">';
-        // echo '<p>'.$title.'<span class="pull-right"> '.++$i.'</span>';
         echo '<p>'.++$i.')<span class=""> '.$folderName.'</span></p>';
         echo '</div>';
         echo '</a>';
         echo '</div>';
         echo '</div>';
     }
+
     unset($folderName);
     unset($folderID);
     unset($parentFolderID);
 }
 $query = null;
 ?>
-
-<!-- Fash Message -->
-<?php if(isset($_SESSION['flash'])): ?>
-    <?php foreach($_SESSION['flash'] as $type => $message): ?>
-        <div class="alert alert-<?php echo $type; ?> alert-anim fade in">
-            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-            <?php echo $message; ?>
-        </div>
-    <?php endforeach; ?>
-    <?php unset($_SESSION['flash']); ?>
-<?php endif; ?>
